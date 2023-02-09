cda6669d0e4bff10fd1eee2c4f18c91f8b676a3f
Casper <casper.dennijs@outlook.com>
2023-02-09 15:06:25 +0100

    Tab card en API gefixed

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const firstname = document.querySelector("span#firstname");
-const lastname = document.querySelector("span#lastname");
+const name = document.querySelector("p.name");
 
 async function getData() {
 	let res = await fetch("https://tribes-api.netlify.app/.netlify/functions/member?id=cldeppc393z9h0avwfpz8hb99")
@@ -8,8 +7,7 @@ async function getData() {
 
 function showData(data) {
     console.log(data);
-    firstname.textContent = data.data.member.name;
-    lastname.textContent = data.data.member.prefix + " " + data.data.member.surname;
+    name.textContent = data.data.member.name;
 }
 
 getData()
